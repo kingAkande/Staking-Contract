b3122aa23f3f212ad042e116b4d675063426e1e5
Akande Sulaimon <kingakande@gmail.com>
2024-08-31 16:39:13 +0100

    update

--- contracts/ERC20Staking.sol
+++ contracts/ERC20Staking.sol
@@ -1,15 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-
-// Interface for the ERC20 Token
-interface IERC20 {
-    function totalSupply() external view returns (uint256);
-    function balanceOf(address account) external view returns (uint256);
-    function transfer(address recipient, uint256 amount) external returns (bool);
-    function allowance(address owner, address spender) external view returns (uint256);
-    function approve(address spender, uint256 amount) external returns (bool);
-    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
-}
+import "./IERC20.sol";
 
 contract ERC20StakingContract {
     IERC20 public stakingToken;  // ERC20 token used for staking
